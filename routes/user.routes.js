74330e7ddd48ed4e158f48d9734c81a524a2230d
MarvinOro <0marvino02@gmail.com>
2025-08-26 15:42:13 -0400

    completed backend: added authentication and authorization for user security,integrated Arcjet for bot detection and rate limiting, subscription addition, reminder workflows, and email sending

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,10 +1,14 @@
-import {Router } from 'express';
+import { Router } from 'express';
+
+import authorize from '../middlewares/auth.middlewares.js';
+
+import { getUsers, getUser } from '../controllers/user.controller.js';
 
 const userRouter = Router();
 
-userRouter.get('/', (req, res) => res.send({ title: 'GET all users'}));
+userRouter.get('/', getUsers);
 
-userRouter.get(':id', (req, res) => res.send({ title: 'GET user details'}));
+userRouter.get('/:id', authorize, getUser);
 
 userRouter.post('/', (req, res) => res.send({ title: 'CREATE a new user'}));
 
